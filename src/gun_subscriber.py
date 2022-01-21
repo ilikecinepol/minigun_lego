b06f94b815edf02f635858974fb6f8e833d2d32d
wfsbear <47589206+ilikecinepol@users.noreply.github.com>
2022-01-22 01:42:55 +0300

    second version - manual mode completed

--- src/gun_subscriber.py
+++ src/gun_subscriber.py
@@ -1,3 +1,9 @@
+GNU
+nano
+2.9
+.3
+src / minigun_lego / src / gun_subscriber.py
+
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -5,9 +11,6 @@ import rospy
 from std_msgs.msg import Float32
 from minigun_lego.msg import gun_message
 import RPi.GPIO as GPIO
-
-import RPi.GPIO as GPIO
-import time
 
 pwm_z = 12
 pwm_y = 13
@@ -34,15 +37,19 @@ GPIO.setup(in2_y, GPIO.OUT)
 GPIO.setup(in3_x, GPIO.OUT)
 GPIO.setup(in4_x, GPIO.OUT)
 GPIO.setup(pwm_x, GPIO.OUT)
-
+k = 30
 z = GPIO.PWM(pwm_z, 50)
 z.start(0)
 y = GPIO.PWM(pwm_y, 50)
 y.start(0)
 
 
+def limiter(value, min=-100, max=100):
+    return min if value < min else max if value > max else value
+
+
 def move(speed_y, speed_z):
-    if speed_y > 0:
+    if speed_y < 0:
         GPIO.output(in1_y, GPIO.HIGH)
         GPIO.output(in2_y, GPIO.LOW)
     else:
@@ -58,22 +65,33 @@ def move(speed_y, speed_z):
     y.ChangeDutyCycle(abs(speed_y))
 
 
-def shot():
-    GPIO.output(in4_x, GPIO.HIGH)
-    GPIO.output(in3_x, GPIO.LOW)
-    GPIO.output(pwm_x, GPIO.HIGH)
+def shot(status):
+    if status == 'trigger':
+        GPIO.output(in4_x, GPIO.HIGH)
+        GPIO.output(in3_x, GPIO.LOW)
+        GPIO.output(pwm_x, GPIO.HIGH)
+    else:
+        GPIO.output(in4_x, GPIO.LOW)
+        GPIO.output(in3_x, GPIO.LOW)
+        GPIO.output(pwm_x, GPIO.LOW)
 
 
 def chatter_callback(message):
-    speed_y = -1 * message.angular_y
-    speed_z = message.angular_z
-    if message.shot == 'trigger':
-        shot()
+    global k
+    # rospy.loginfo(message.status)
+    speed_z = limiter(k * message.angular_y)
+    speed_y = limiter(k * message.angular_z)
+    # if message.shot == 'trigger':
+    shot(message.shot)
     move(speed_y, speed_z)
 
 
-rospy.init_node('gun_subscriber', anonymous=True)
+def listener():
+    rospy.init_node('gun_subscriber', anonymous=True)
+    rospy.Subscriber('robot_speed_topic', gun_message, chatter_callback)
+    rospy.spin()
 
-rospy.Subscriber('robot_speed_topic', gun_message, chatter_callback)
-rospy.spin()
+
+if __name__ == '__main__':
+    listener()
 
